f3a231dbef2179d6612dc515130865a03b00749c
Raphael Zaghroun <raphael.zaghroun@etu.minesparis.psl.eu>
2024-10-18 17:18:38 +0200

    Add weighted training function with example

--- unet3d/train.py
+++ unet3d/train.py
@@ -10,6 +10,7 @@ from unet3d.collate import pad_collate
 from unet3d.dataset import BaselineDataset, NoCloudDataset
 from unet3d.unet3d import UNet
 
+from unet3d.weights import W, W_inv
 
 def print_iou_per_class(
     targets: torch.Tensor,
@@ -132,9 +133,86 @@ def train_model(
     return model
 
 
+def weighted_train_model(
+    data_folder: Path,
+    nb_classes: int,
+    input_channels: int,
+    num_epochs: int = 10,
+    batch_size: int = 4,
+    learning_rate: float = 1e-3,
+    device: str = "cpu",
+    verbose: bool = False,
+    weights: torch.tensor = None,
+) -> UNet:
+    """
+    Training pipeline.
+    """
+    # Create data loader
+    dataset = NoCloudDataset(data_folder)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, collate_fn=pad_collate, shuffle=True
+    )
+
+    # Initialize the model, loss function, and optimizer
+    model = UNet(input_channels, nb_classes,dim=3)
+    if weights:
+        criterion = nn.CrossEntropyLoss(weight=weights)
+    else:
+        criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Move the model to the appropriate device (GPU if available)
+    device = torch.device(device)
+    model.to(device)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        model.train()  # Set the model to training mode
+        running_loss = 0.0
+
+        for i, (inputs, targets) in tqdm(enumerate(dataloader), total=len(dataloader)):
+            # Move data to device
+            inputs["S2"] = inputs["S2"].to(device)  # Satellite data
+            targets = targets.to(device)
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(inputs["S2"]) 
+            outputs_median_time = torch.median(outputs,2).values
+
+            # Loss computation
+            loss = criterion(outputs_median_time, targets)
+
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            # Get the predicted class per pixel (B, H, W)
+            preds = torch.argmax(outputs_median_time, dim=1)
+
+            # Move data from GPU/Metal to CPU
+            targets = targets.cpu().numpy().flatten()
+            preds = preds.cpu().numpy().flatten()
+
+            if verbose:
+                # Print IOU for debugging
+                print_iou_per_class(targets, preds, nb_classes)
+                print_mean_iou(targets, preds)
+
+        # Print the loss for this epoch
+        epoch_loss = running_loss / len(dataloader)
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}")
+
+    print("Training complete.")
+    return model
+
 if __name__ == "__main__":
     # Example usage:
-    model = train_model(
+    weights = torch.tensor(list(W_inv.values()))
+    model = weighted_train_model(
         data_folder=Path(
             "/Users/louis.stefanuto.c/Documents/pastis-benchmark-mines2024/DATA/TRAIN/"
         ),
@@ -145,4 +223,5 @@ if __name__ == "__main__":
         learning_rate=1e-3,
         device="mps",
         verbose=True,
+        weights=weights,
     )
